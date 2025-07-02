ff20f1d1a0eff029f03570531ed39e866be42ff4
Unknow-Error <salocin-terlosaav@outlook.com>
2025-07-02 10:46:49 -0300

    modified:   lib/CitrusTensorial/CitraMatriz.hpp 	modified:   lib/CitrusTensorial/CitraVector.hpp

--- lib/CitrusTensorial/CitraMatriz.hpp
+++ lib/CitrusTensorial/CitraMatriz.hpp
@@ -1,5 +1,6 @@
 
 #include "../CitrusTensor.hpp"
+#include "../CitraVector.hpp"
 
 // Una matriz es un Tensor de tipo (1,1). Multidimensional, pero siempre rango 2.
 template<typename numTipo> 
@@ -27,13 +28,22 @@ public:
     void setNumFilas(const size_t numFilas);
     void setNumColumnas(const size_t numColumnas);
 
+    //Operaciones para matrices y de algebra lineal
     Matriz<numTipo> sumaMatricial(const Matriz<numTipo>& matriz1, const Matriz<numTipo>& matriz2) const;
     Matriz<numTipo> restaMatricial(const Matriz<numTipo>& matriz1, const Matriz<numTipo>& matriz2) const;
     Matriz<numTipo> productoMatricial(const Matriz<numTipo>& matriz1, const Matriz<numTipo>& matriz2) const;
     Matriz<numTipo> productoKronecker(const Matriz<numTipo>& matriz1, const Matriz<numTipo>& matriz2) const;
     numTipo traza(const Matriz<numTipo>& matriz) const;
+
+    //Operaciones para la inversa
     Matriz<numTipo> matrizAdjunta(const Matriz<numTipo>& matriz) const;
     numTipo determinante(const Matriz<numTipo>& matriz) const;
     Matriz<numTipo> transpuesta(const Matriz<numTipo>& matriz) const;
     Matriz<numTipo> inversa(const Matriz<numTipo>& matriz) const;
+
+    //Operaciones diagonalización
+    const numTipo* eigenValores(const Matriz<numTipo>& matriz) const;
+    const Vector<numTipo>* eigenVectores(const Matriz<numTipo>& matriz) const;
+    Matriz<numTipo> matrizEigenVectores(const Matriz<numTipo>& matriz) const;
+    Matriz<numTipo> matrizDiagonal(const Matriz<numTipo>& matriz) const;
 };
--- lib/CitrusTensorial/CitraVector.hpp
+++ lib/CitrusTensorial/CitraVector.hpp
@@ -1,12 +1,39 @@
 
-#include "../CitraMatriz.hpp"
+#include "../CitrusTensor.hpp"
 
+// Una vector es un Tensor de tipo (0,1) fila o tipo (1,0) columna. Multidimensional, pero siempre rango 1.
 template<typename numTipo>
 struct Vector{
-
+    Tensor<numTipo> vector;
+    vector.rango = 1;
 };
 
 template<typename numTipo>
 class CitraVector : CitrusTensor<numTipo> {
+private:
+    Vector<numTipo> vec;
+public:
+    //Constructores
+    CitraVector();
+    CitraVector(const size_t tamanio_, bool tipo_);
 
+    //Destructor
+    ~CitraVector();
+
+    //Operaciones vectoriales.
+    Vector<numTipo> sumaVectorial(const Vector<numTipo>& vector1, const Vector<numTipo>& vector2) const;
+    Vector<numTipo> restaVectorial(const Vector<numTipo>& vector1, const Vector<numTipo>& vector2) const;
+    Vector<numTipo> productoEscalar(const Vector<numTipo>& vector1, const Vector<numTipo>& vector2) const;
+    Vector<numTipo> productoVectorial(const Vector<numTipo>& vector1, const Vector<numTipo>& vector2) const;
+    Vector<numTipo> proyeccionEscalar(const Vector<numTipo>& vector1, const Vector<numTipo>& vector2) const;
+    Vector<numTipo> proyeccionVectorial(const Vector<numTipo>& vector1, const Vector<numTipo>& vector2) const;
+
+    //Geometria lineal (Euclidiana)
+    numTipo distEuclidianaInterPuntos(const Vector<numTipo>& punto1, const Vector<numTipo>& punto2) const; //Distancia Euclideana
+    Vector<numTipo> vectorDirector(const Vector<numTipo>& punto1, const Vector<numTipo>& punto2) const;
+    const Vector<numTipo>* recta(const Vector<numTipo>& vectorDirector, const Vector<numTipo>& punto) const;
+    numTipo distEuclidianaInterRectas(const Vector<numTipo>* recta1, const Vector<numTipo>* recta2) const;
+    numTipo distEuclidianaInterRectaPunto(const Vector<numTipo>* recta, const Vector<numTipo> punto) const;
+    Vector<numTipo> vectorNormal(const Vector<numTipo>& vectorDirector1, const Vector<numTipo>& vectorDirector2) const;
+    const Vector<numTipo>* plano(const Vector<numTipo>& normal, const Vector<numTipo>& punto1) const;
 };
